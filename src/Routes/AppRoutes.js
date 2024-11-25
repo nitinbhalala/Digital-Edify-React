904bddb4f4e7c0b7c54261ea0e4f9df37657b6fc
Nitin Bhalala <bhalalanitin5291@gmail.com>
2024-11-25 11:26:16 +0530

    course page add

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from '../Pages/Home';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../Pages/Home";
+import Course from "../Pages/Course";
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/course" element={<Course />} />
     <Route path="*" element={<Home />} />
   </Routes>
 );
